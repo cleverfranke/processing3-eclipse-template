f761947ee5222dded03132ef319305b1be29169c
Wilco Tomassen <wilco@cleverfranke.com>
2016-06-08 13:51:42 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+Processing 3 Eclipse template
+-----------------------------
+
+Small template to get Processing 3 quickly up and running in Eclipse
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+	
+	public void settings() {
+		size(1024, 768);
+	}
+	
+	public void setup() {
+		// Add setup code here
+	}
+	
+	public void draw() {
+		// Add draw code here
+	}
+	
+	public static void main(String[] args) {
+		PApplet.main(Main.class.getName());
+	}
+
+}
